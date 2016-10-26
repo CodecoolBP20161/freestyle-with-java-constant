ac5cf625deed5630b0cba785e06a589a1456251f
vbenedek <xbenedek01@gmail.com>
2016-10-26 12:03:16 +0200

    example data generating

--- src/Build.java
+++ src/Build.java
@@ -5,11 +5,13 @@ public class Build {
     public static void main( String args[] ) {
         Play.dropPlayTable();
         Play.buildPlayTable();
-        Play a = new Play(1, "1540", "Egy", "Moni");
-        Play b = new Play(2, "1530", "Ketto", "Moni");
-        Play c = new Play(3, "1530", "Harom", "Moni");
+        Play a = new Play(1, "oct. 26. 19:00", "János Vitéz", "Thália Színház");
+        Play b = new Play(2, "oct. 26. 19:00", "A férfiagy - avagy nincs itt valami ellentmondás?", "Thália Színház");
+        Play h = new Play(9, "oct. 1. 19:00", "Random mű", "Thália színház");
+        Play i = new Play(10, "jun. 1. 19:00", "Random mű2", "Thália színház");
         a.insertToTable();
         b.insertToTable();
-        c.insertToTable();
+        h.insertToTable();
+        i.insertToTable();
     }
 }
