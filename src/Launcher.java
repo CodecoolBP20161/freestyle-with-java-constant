fafc0c456eadbb45ed31d008721b693cb3ee88c7
vbenedek <xbenedek01@gmail.com>
2016-10-26 12:01:36 +0200

    test print removed

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 public class Launcher {
     public static void main( String args[] ) throws Exception {
         ArrayList<Play> playList = Play.selectAll();
-        for (Play p: playList)
-            System.out.println(p.time);
-
         TableFromPostgreSQLDatabase frame = new TableFromPostgreSQLDatabase();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Constant");
